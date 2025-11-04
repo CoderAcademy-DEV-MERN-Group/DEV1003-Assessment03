edf2edc67e46f61a85e11db4b4d58ffb51347192
Jordan Leal-Walker <16386@coderacademy.edu.au>
2025-11-04 17:00:20 +1100

    feat(apiServices): created axios api wrappers to handle fetch requests

---
+++ src/utilities/services/apiServices.jsx
@@ -0,0 +1,277 @@
+import axios from "axios";
+
+// Create a custom axios instance with custom configuration
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL, // Will set url depending on run command
+  timeout: 10000, // 10 seconds timeout for requests to prevent hanging
+  headers: { "Content-Type": "application/json" }, // Defaults headers to JSON format
+});
+
+/*
+{ This is the full axios response object
+  data: { ... }, <- The actual backend response data from our API
+  status: 201,
+  statusText: 'Created',
+  headers: { ... },
+  config: { ... },
+  request: { ... }
+}
+*/
+
+// Setup interceptors (like middleware) for requests and responses to handle auth token automatically
+
+// Request interceptor adds JWT token from localStorage to header if exists
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("authToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+// ------------------------------------------------------------------------------------------------
+// AUTH ROUTES
+
+// Register a new user
+export const registerUser = async (userBodyData) => {
+  try {
+    // Post takes route suffix as first arg and body data as second arg
+    const res = await api.post("/auth/register", userBodyData);
+    // Automatically parses JSON response. 'Data' contains the actual response body from backend
+    return res.data;
+    // Catch error, log it and re-throw for further handling
+  } catch (err) {
+    console.error(`Error registering user: ${err}`);
+    throw err;
+  }
+};
+
+// Login an existing user
+export const loginUser = async (userBodyData) => {
+  try {
+    const res = await api.post("/auth/login", userBodyData);
+    return res.data;
+  } catch (err) {
+    console.error(`Error logging in user: ${err}`);
+    throw err;
+  }
+};
+
+// Logout current user
+export const logoutUser = async () => {
+  try {
+    const res = await api.post("/auth/logout");
+    return res.data;
+  } catch (err) {
+    // Shouldn't fail since no controller logic for logout route, but just in case
+    console.error(`Error logging out user: ${err}`);
+    throw err;
+  }
+};
+
+// ------------------------------------------------------------------------------------------------
+// USER ROUTES
+
+// Get all users
+export const getAllUsers = async () => {
+  try {
+    const res = await api.get("/users");
+    return res.data;
+  } catch (err) {
+    console.error(`Error fetching users: ${err}`);
+    throw err;
+  }
+};
+
+// Get current logged-in user details
+export const getCurrentUser = async () => {
+  try {
+    const res = await api.get("/users/my-profile");
+    return res.data;
+  } catch (err) {
+    console.error(`Error fetching current user: ${err}`);
+    throw err;
+  }
+};
+
+// Update current logged-in user details
+export const updateCurrentUser = async (updateBodyData) => {
+  try {
+    const res = await api.put("/users/my-profile", updateBodyData);
+    return res.data;
+  } catch (err) {
+    console.error(`Error updating current user: ${err}`);
+    throw err;
+  }
+};
+
+// Update current logged-in user password
+export const updateCurrentUserPassword = async (passwordBodyData) => {
+  try {
+    const res = await api.put("/users/my-profile/update-password", passwordBodyData);
+    return res.data;
+  } catch (err) {
+    console.error(`Error updating current user password: ${err}`);
+    throw err;
+  }
+};
+
+// Delete current logged-in user
+export const deleteCurrentUser = async () => {
+  try {
+    const res = await api.delete("/users/my-profile");
+    return res.data;
+  } catch (err) {
+    console.error(`Error deleting current user: ${err}`);
+    throw err;
+  }
+};
+
+// ------------------------------------------------------------------------------------------------
+// FRIENDSHIP ROUTES
+
+// Get all friendship documents for logged-in user
+export const getAllFriendships = async () => {
+  try {
+    const res = await api.get("/friendships/my-friends");
+    return res.data;
+  } catch (err) {
+    console.error(`Error fetching friendships: ${err}`);
+    throw err;
+  }
+};
+
+// Create new pending friendship document (send friend request)
+export const createFriendship = async (recipientUserId) => {
+  try {
+    const res = await api.post(`/friendships/${encodeURIComponent(recipientUserId)}`);
+    return res.data;
+  } catch (err) {
+    console.error(`Error creating friendship: ${err}`);
+    throw err;
+  }
+};
+
+// Update existing pending friendship document (accept friend request)
+export const updateFriendship = async (requesterUserId) => {
+  try {
+    const res = await api.put(`/friendships/my-friends/${encodeURIComponent(requesterUserId)}`);
+    return res.data;
+  } catch (err) {
+    console.error(`Error updating friendship: ${err}`);
+    throw err;
+  }
+};
+
+// Delete existing friendship document (unfriend or reject friend request)
+export const deleteFriendship = async (otherUserId) => {
+  try {
+    const res = await api.delete(`/friendships/my-friends/${encodeURIComponent(otherUserId)}`);
+    return res.data;
+  } catch (err) {
+    console.error(`Error deleting friendship: ${err}`);
+    throw err;
+  }
+};
+
+// ------------------------------------------------------------------------------------------------
+// MOVIES ROUTES
+
+// Get all reel-canon movies
+export const getAllMovies = async () => {
+  try {
+    const res = await api.get("/movies/reel-canon");
+    return res.data;
+  } catch (err) {
+    console.error(`Error fetching movies: ${err}`);
+    throw err;
+  }
+};
+
+// Get a single movie by title query
+export const getMovieByTitle = async (title) => {
+  try {
+    /* Axios params (second argument) safely handles URL encoding. Is equivalent to using
+    `/movies/search?title=${encodeURIComponent(title)}` */
+    const res = await api.get(`/movies/search`, { params: { title } });
+    return res.data;
+  } catch (err) {
+    console.error(`Error fetching movie by title: ${err}`);
+    throw err;
+  }
+};
+
+// Get a single movie by IMDB ID
+export const getMovieByImbdId = async (imbdId) => {
+  try {
+    const res = await api.get(`/movies/${encodeURIComponent(imbdId)}`);
+    return res.data;
+  } catch (err) {
+    console.error(`Error fetching movie by IMBD ID: ${err}`);
+    throw err;
+  }
+};
+
+// ------------------------------------------------------------------------------------------------
+// REEL-PROGRESS ROUTES
+
+// Get reel-progress array for logged in user
+export const getUserReelProgress = async () => {
+  try {
+    const res = await api.get("/reel-progress");
+    return res.data;
+  } catch (err) {
+    console.error(`Error fetching user reel-progress: ${err}`);
+    throw err;
+  }
+};
+
+// Update reel-progress by adding a new movie to reel-progress array
+export const addMovieToReelProgress = async (movieBodyData) => {
+  try {
+    const res = await api.post("/reel-progress", movieBodyData);
+    return res.data;
+  } catch (err) {
+    console.error(`Error adding movie to user reel-progress: ${err}`);
+    throw err;
+  }
+};
+
+// Update rating of movie in reel-progress array for logged in user
+export const updateReelProgressMovieRating = async (movieId, movieRating) => {
+  try {
+    const res = await api.patch(`/reel-progress/${encodeURIComponent(movieId)}`, movieRating);
+    return res.data;
+  } catch (err) {
+    console.error(`Error updating movie rating in user reel-progress: ${err}`);
+    throw err;
+  }
+};
+
+// Delete movie from reel-progress array for logged in user
+export const deleteMovieFromReelProgress = async (movieId) => {
+  try {
+    const res = await api.delete(`/reel-progress/${encodeURIComponent(movieId)}`);
+    return res.data;
+  } catch (err) {
+    console.error(`Error deleting movie from user reel-progress: ${err}`);
+    throw err;
+  }
+};
+
+// ------------------------------------------------------------------------------------------------
+// LEADERBOARD ROUTE
+
+// Get leaderboard data for all users
+export const getLeaderboard = async () => {
+  try {
+    const res = await api.get("/leaderboard");
+    return res.data;
+  } catch (err) {
+    console.error(`Error fetching leaderboard data: ${err}`);
+    throw err;
+  }
+};
