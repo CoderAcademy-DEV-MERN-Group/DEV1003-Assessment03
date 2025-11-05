84f037d6bd828e7bfbaf01501f988a4b98396cc5
Jordan Leal-Walker <16386@coderacademy.edu.au>
2025-11-05 22:06:27 +1100

    feat(authProvider): created authProvider component to provide global user data and login/logout functionality to all app components

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import "./styles/index.css";
 import App from "./App";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Modal from "react-modal";
+import AuthProvider from "./contexts/authProvider";
 
 Modal.setAppElement("#root");
 
@@ -20,7 +21,9 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </QueryClientProvider>
   </StrictMode>
 );
