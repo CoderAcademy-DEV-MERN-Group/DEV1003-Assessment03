98c38fa03865e0d3f44f56b348f817e544150092
Joss <joss.raine@gmail.com>
2025-11-05 16:05:46 +1100

    feat: scaffold for navbar with navlinks created. will implement auth logic for signin/signout display once created

--- src/components/navbarHeader.jsx
+++ src/components/navbarHeader.jsx
@@ -1,4 +1,4 @@
-import clsx from "clsx";
+// import clsx from "clsx";
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import logo from "../assets/images/navbarlogo215px.png";
@@ -18,21 +18,44 @@ function Header() {
       </NavLink>
 
       {/* Link section */}
-      <section>
-        <>Home</>
-        <>My Profile</>
-        <>Leaderboard</>
-        <>Register</>
-        <>The Reel Canon</>
+      <section className={styles.pageLinks}>
+        <NavLink to="/" className={({ isActive }) => (isActive ? styles.activeLink : styles.link)}>
+          Home
+        </NavLink>
+        <NavLink
+          to="/profile"
+          className={({ isActive }) => (isActive ? styles.activeLink : styles.link)}
+        >
+          My Profile
+        </NavLink>
+        <NavLink
+          to="/leaderboard"
+          className={({ isActive }) => (isActive ? styles.activeLink : styles.link)}
+        >
+          Leaderboard
+        </NavLink>
+        <NavLink
+          to="/register"
+          className={({ isActive }) => (isActive ? styles.activeLink : styles.link)}
+        >
+          Register
+        </NavLink>
+        <NavLink
+          to="/the-reel-canon"
+          className={({ isActive }) => (isActive ? styles.activeLink : styles.link)}
+        >
+          The Reel Canon
+        </NavLink>
       </section>
 
-      <section>
-        <button onClick={() => setShowLogin(true)} className="open-login-button">
+      {/* Modal buttons will need to be adjusted when authContext has been completed for now it's fine as is. */}
+      <section className={styles.modalButtons}>
+        <button onClick={() => setShowLogin(true)} className={styles.openLoginButton}>
           Sign In
         </button>
         <Login isOpen={showLogin} onClose={() => setShowLogin(false)} />
-        <button onClick={() => setShowLogout(true)} className="open-logout-button">
-          Log Out
+        <button onClick={() => setShowLogout(true)} className={styles.openLogoutButton}>
+          Sign Out
         </button>
         <Logout isOpen={showLogout} onClose={() => setShowLogout(false)} />
       </section>
