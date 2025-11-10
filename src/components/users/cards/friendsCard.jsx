3d42ffb8ec7264c3f1b5b23f8ababafe6b70fc79
Joss <joss.raine@gmail.com>
2025-11-10 23:04:54 +1100

    feat: moved user buttons from card displays to above cards, this looks cleaner. have set up user friendship table for friendship data still to style, and have second card ready for friend search and add features

---
+++ src/components/users/cards/friendsCard.jsx
@@ -0,0 +1,45 @@
+import { useAllFriendships, useAllUsers } from "../../../utilities/customHooks";
+import styles from "../UserComponents.module.scss";
+
+export default function FriendsCard({ className }) {
+  const { data: friendships } = useAllFriendships();
+  const { data: users } = useAllUsers();
+
+  const usersLookup = users?.users?.reduce((account, user) => {
+    account[user._id] = user;
+    return account;
+  }, {});
+
+  return (
+    <section className={className}>
+      <fieldset className={styles.cardBorder}>
+        <legend>Your Friendships</legend>
+        {!friendships?.friendships || friendships.friendships.length === 0 ? (
+          <p>No friends yet!</p>
+        ) : (
+          <table className={styles.friendsTable}>
+            <thead>
+              <tr>
+                <th className={styles.colUsername}>Username</th>
+                <th className={styles.colEmail}>Username</th>
+                <th className={styles.colStatus}>Username</th>
+              </tr>
+            </thead>
+            <tbody>
+              {friendships.friendships.map((friendship) => {
+                const friendUser = usersLookup?.[friendship.user2];
+                return (
+                  <tr key={friendship._id} className={styles.tableRow}>
+                    <td>Username: {friendUser?.username || "Uknown user"}</td>
+                    <td>Email: {friendUser?.email}</td>
+                    <td>Status: {!friendship.friendRequestAccepted ? "Pending" : "Accepted"}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        )}
+      </fieldset>
+    </section>
+  );
+}
--- src/components/users/cards/userInfoCard.jsx
+++ src/components/users/cards/userInfoCard.jsx
@@ -1,22 +1,15 @@
-import { useMemo, useState } from "react";
+import { useMemo } from "react";
 import { useAuthContext } from "../../../contexts/useAuthContext";
 import { useAllMovies, useUserReelProgress } from "../../../utilities/customHooks";
 import { getFavouriteGenreStats } from "../tabs/myStatsTab";
 import LoadingSpinner from "../../common/LoadingScreenOverlay";
 import styles from "../UserComponents.module.scss";
 import MoviesWatched from "../tabs/moviesWatchedTab";
-import UpdateProfile from "../../modals/UpdateProfileInfo";
-import UpdatePassword from "../../modals/UpdatePassword";
-import DeleteUser from "../../modals/DeleteUser";
 
 export default function UserInfoCard({ className }) {
   const { data, isLoading } = useAllMovies();
   const { user } = useAuthContext();
   const { data: userRp } = useUserReelProgress();
-
-  const [showDeleteProfile, setShowDeleteProfile] = useState(false);
-  const [showUpdateProfile, setShowUpdateProfile] = useState(false);
-  const [showUpdatePassword, setShowUpdatePassword] = useState(false);
 
   const topGenres = useMemo(() => {
     if (!userRp?.reelProgress || !data?.movies) return [];
@@ -29,19 +22,6 @@ export default function UserInfoCard({ className }) {
 
   return (
     <section className={className}>
-      <button onClick={() => setShowUpdateProfile(true)} className={styles.navItem}>
-        {" "}
-        Edit Profile Info{" "}
-      </button>{" "}
-      |
-      <button onClick={() => setShowUpdatePassword(true)} className={styles.navItem}>
-        {" "}
-        Update Password{" "}
-      </button>
-      <button onClick={() => setShowDeleteProfile(true)} className={styles.deleteButton}>
-        {" "}
-        Delete Profile{" "}
-      </button>
       <fieldset className={styles.cardBorder}>
         <legend>Your Cinematic Info</legend>
         <h2>Username: {user.username}</h2>
@@ -53,9 +33,6 @@ export default function UserInfoCard({ className }) {
           <div>You haven't rated any movies yet!</div>
         )}
         <MoviesWatched />
-        <UpdateProfile isOpen={showUpdateProfile} onClose={() => setShowUpdateProfile(false)} />
-        <UpdatePassword isOpen={showUpdatePassword} onClose={() => setShowUpdatePassword(false)} />
-        <DeleteUser isOpen={showDeleteProfile} onClose={() => setShowDeleteProfile(false)} />
       </fieldset>
     </section>
   );
