6bd01b6d6f0cf53cd7ff475ab30c15e1f0d7fafa
Joss <joss.raine@gmail.com>
2025-11-12 10:03:59 +1100

    fix: removed chagnes from leaderboard code along

--- src/components/users/cards/AddFriendCard.jsx
+++ src/components/users/cards/AddFriendCard.jsx
@@ -1,48 +1,45 @@
-import { useMemo } from "react";
-import { useAuthContext } from "../../../contexts/useAuthContext";
-import { useAllFriendships, useAllUsers } from "../../../utilities/customHooks";
-import { style } from "framer-motion/client";
+// import { useMemo } from "react";
+// import { useAuthContext } from "../../../contexts/useAuthContext";
+// import { useAllFriendships, useAllUsers } from "../../../utilities/customHooks";
+// import styles from "../UserComponents.module.scss";
 
-export default function AddFriendCard({ className }) {
-  const { user: currentUser } = useAuthContext();
-  const { data: allUsersData } = useAllUsers();
-  const { data: allFriendshipsData } = useAllFriendships();
+// export default function AddFriendCard({ className }) {
+//   const { user: currentUser } = useAuthContext();
+//   const { data: allUsersData } = useAllUsers();
+//   const { data: allFriendshipsData } = useAllFriendships();
 
-  // Extract arrays from API response objects
-  const allUsers = allUsersData?.users;
-  const allFriendships = allFriendshipsData?.friendships;
+//   // Extract arrays from API response objects
+//   const allUsers = allUsersData?.users;
+//   const allFriendships = allFriendshipsData?.friendships;
 
-  function findUnfriendedUsers() {
-    // Return an empty array if needed data isn't loaded yet
-    if (!allUsers || !allFriendships || !currentUser) return [];
-    // Make set of all friends ID's, set using .has() is faster for lookups than array .includes()
-    const friendIds = new Set(
-      allFriendships.map((e) => (e.user1 === currentUser._id ? e.user2 : e.user1))
-    );
-    // Return array of users from allUsers that aren't current user or in friendIds
-    return allUsers.filter((e) => e._id !== currentUser._id && !friendIds.has(e._id));
-  }
-  // useMemo only recalculates when dependencies change, more efficient than useEffect + useState
-  const unfriendedUsers = useMemo(findUnfriendedUsers, [allUsers, allFriendships, currentUser]);
+//   function findUnfriendedUsers() {
+//     // Return an empty array if needed data isn't loaded yet
+//     if (!allUsers || !allFriendships || !currentUser) return [];
+//     // Make set of all friends ID's, set using .has() is faster for lookups than array .includes()
+//     const friendIds = new Set(
+//       allFriendships.map((e) => (e.user1 === currentUser._id ? e.user2 : e.user1))
+//     );
+//     // Return array of users from allUsers that aren't current user or in friendIds
+//     return allUsers.filter((e) => e._id !== currentUser._id && !friendIds.has(e._id));
+//   }
+//   // useMemo only recalculates when dependencies change, more efficient than useEffect + useState
+//   const unfriendedUsers = useMemo(findUnfriendedUsers, [allUsers, allFriendships, currentUser]);
 
-  const handleSendRequest = (user) => {
-    console.log("Sending friend request to:", user.username);
-  };
+//   const handleSendRequest = (user) => {
+//     console.log("Sending friend request to:", user.username);
+//   };
 
-  return (
-    <section className={className}>
-      <article className={cardBorder}>
-        <h2>Add Friends</h2>
-        {/* Search bar!!!! WEEEWWW!!!! */}
-        <search className={styles.searchCointainer}>
-          <input
-            type="text"
-            className={styles.searchField}
-            placeholder="Search by username..."
-            value={searchTerm}
-          ></input>
-        </search>
-      </article>
-    </section>
-  );
-}
+//   return (
+//     <section className={className}>
+//       <article className={styles.cardBorder}>
+//         <h2>Add Friends</h2>
+//         {/* Search bar!!!! WEEEWWW!!!! */}
+//         <search className={styles.searchCointainer}>
+//           <input type="text" className={styles.searchField} placeholder="Search by username...">
+//             sd
+//           </input>
+//         </search>
+//       </article>
+//     </section>
+//   );
+// }
--- src/pages/userProfile/userProfile.jsx
+++ src/pages/userProfile/userProfile.jsx
@@ -6,7 +6,7 @@ import styles from "./userProfile.module.scss";
 import UpdateProfile from "../../components/modals/UpdateProfileInfo";
 import UpdatePassword from "../../components/modals/UpdatePassword";
 import DeleteUser from "../../components/modals/DeleteUser";
-import AddFriendCard from "../../components/users/cards/AddFriendCard";
+// import AddFriendCard from "../../components/users/cards/AddFriendCard";
 
 export default function UserProfile() {
   const [showDeleteProfile, setShowDeleteProfile] = useState(false);
@@ -29,7 +29,7 @@ export default function UserProfile() {
       <div className={styles.userCards}>
         <UserInfoCard className={styles.userInfoCard} />
         <FriendsCard className={styles.userFriendsCard} />
-        <AddFriendCard className={styles.userFriendsCard} />
+        {/* <AddFriendCard className={styles.userFriendsCard} /> */}
         <ReelProgressCard className={styles.userReelProgressCard} />
       </div>
       <UpdateProfile isOpen={showUpdateProfile} onClose={() => setShowUpdateProfile(false)} />
