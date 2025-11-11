40ab00a3aae11d82380714dda3e79ec244d24d14
Nhi Huynh <nhi.huynh@hotmail.com>
2025-11-11 19:32:29 +0800

    fix(useLeaderboard): remove small typo '?'

--- src/utilities/customHooks/useLeaderboard.js
+++ src/utilities/customHooks/useLeaderboard.js
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { getLeaderboard } from "../services/apiServices";
 
-// Create tanstack fetch? custom hook to GET leaderboard data
+// Create tanstack fetch custom hook to GET leaderboard data
 export const useLeaderboard = () =>
   useQuery({
     queryKey: ["leaderboard"],
