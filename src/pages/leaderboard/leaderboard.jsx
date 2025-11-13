59584b7a04ef2f73cf05d9325a8506a914539d49
Joss <joss.raine@gmail.com>
2025-11-13 18:51:18 +1100

    feat: styling leaderboard for desktop at basic level completed

--- src/pages/leaderboard/leaderboard.jsx
+++ src/pages/leaderboard/leaderboard.jsx
@@ -1,16 +1,21 @@
 import LeaderboardPodium from "../../components/common/leaderboardPodium";
 import RankingList from "./rankingList";
 import { useLeaderboard } from "../../utilities/customHooks";
+import styles from "./leaderboard.module.scss";
 
 export default function Leaderboard() {
   const { data } = useLeaderboard();
   const topRankings = data?.reelProgressData.slice(0, 3).map((e) => e._id) || [];
 
   return (
-    <>
-      <h1>Leaderboard</h1>
-      <LeaderboardPodium rankings={topRankings} />
-      <RankingList title="Ranking List" />
-    </>
+    <section className={styles.leaderboard}>
+      <article className={styles.title}>
+        <h1>Leaderboard</h1>
+      </article>
+      <article className={styles.featureContainer}>
+        <LeaderboardPodium className={styles.podium} rankings={topRankings} />
+        <RankingList className={styles.list} title="Ranking List" />
+      </article>
+    </section>
   );
 }
